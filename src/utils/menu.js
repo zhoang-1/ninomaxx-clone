6a836022529d3edc78cb285e70ae0872334d2503
zhoang-2 <huyhoangzzzzb@gmail.com>
2024-05-02 00:30:11 +0700

    fea: header

---
+++ src/utils/menu.js
@@ -0,0 +1,30 @@
+export const navbarTop = [
+    {
+        name: 'SHOP',  link: 'shop'
+    },{
+        name:'NINOMAXX', link: 'ninomaxx'
+    },{
+        name:'N&M', link: 'n&m'
+    },{
+        name:'N&M CLUB', link: 'n&m-club'
+    },{
+        name:'SWEET SIXTEEN', link: 'sweet-sixteen'
+    }   
+]
+export const navbarBottom = [
+    {
+        name: 'NEW',  link: 'new'
+    },{
+        name: 'BRANDS',  link: 'brands'
+    },{
+        name: 'NAM',  link: 'nam'
+    },{
+        name: 'NỮ',  link: 'nu'
+    },{
+        name: 'TRẺ EM',  link: 'tre-em'
+    },{
+        name: 'STORES',  link: 'stores'
+    },{
+        name: 'MID SEASON SALE',  link: 'mid-season-sale'
+    }
+]
