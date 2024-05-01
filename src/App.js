78757cad2d91719363c3c32a2e2015e585774333
zhoang-2 <huyhoangzzzzb@gmail.com>
2024-05-01 22:42:35 +0700

    fea: layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes, matchPath, useLocation } from 'react-router-dom';
+import { PublicRouter } from './routers/index';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const MainLayout = ({ children }) => {
+        //url path của trang web
+        const location = useLocation();
+
+        //khai báo url path không xài header footer
+        const excludeRoutes = ['/*/:id'];
+
+        // kiểm tra bằng vòng lặp
+        const isExist = excludeRoutes.some((routes) => {
+            // trả về liệu có tồn tại location nào khớp với phần tử url nào trong excludeRoutes hay không
+            return matchPath({ path: routes, exact: true }, location.pathname);
+        });
+
+        // nếu có thì trả về children không xài header và footer
+        if (isExist) {
+            return <>{children}</>;
+        }
+        //nếu không thì trả về children xài cùng header và footer
+        return (
+            <>
+                <Header />
+                {children}
+                <Footer />
+            </>
+        );
+    };
+    return (
+        <BrowserRouter>
+            <MainLayout>
+                <Routes>
+                    {PublicRouter.map((route, index) => {
+                        const Pages = route.component;
+                        return <Route key={index} path={route.path} element={<Pages />} />;
+                    })}
+                </Routes>
+            </MainLayout>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/routers/index.js
@@ -0,0 +1,2 @@
+import Home from '../components/Home/HomeScreen';
+export const PublicRouter = [{ path: '/', component: Home }];
